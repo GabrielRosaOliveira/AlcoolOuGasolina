4a0bfc4ede639a56d23ee6df4ffcc74c2103fb42
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-09-25 20:59:50 -0300

    troque a cor da label

--- AppAlcoolOuGasolina/AppAlcoolOuGasolina/Calculo/ViewController.swift
+++ AppAlcoolOuGasolina/AppAlcoolOuGasolina/Calculo/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var priceAlcool: UITextField!
     @IBOutlet weak var priceGasolina: UITextField!
@@ -20,15 +20,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configsButtonAndLabel()
+        priceAlcool.delegate = self
+        priceGasolina.delegate = self
     }
+    
+//    func calcularCombustivel() {
+//        var calculo: Double = Double(Double(priceAlcool) / Double(priceGasolina))
+//        if calculo < 0.70 {
+//            print(titleLabel)
+//        } else {
+//            print("oi")
+//        }
+//    }
 
     func configsButtonAndLabel() {
+        calcPriceButton.backgroundColor = .blue
+        calcPriceButton.tintColor = .white
         calcPriceButton.setTitle("Calcular", for: .normal)
+        calcPriceButton.layer.cornerRadius = 20
+        
         titleLabel.text = "Digite os Preços!"
-        titleLabel.backgroundColor = .systemBlue
+        titleLabel.backgroundColor = .blue
         titleLabel.textColor = .white
+        
+        priceAlcool.placeholder = "Ex:1.99"
+        priceGasolina.placeholder = "Ex:2.99"
+        priceAlcool.textAlignment = .center
+        priceGasolina.textAlignment = .center
        
     }
-
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        <#code#>
+//    }
+//    
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        <#code#>
+//    }
+//    
 }
 
